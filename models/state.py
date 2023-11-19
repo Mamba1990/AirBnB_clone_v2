d3a51e7893785c3a519c2931b758029e8a635632
Mamba1990 <h.daoudim@outlook.com>
2023-11-19 08:56:34 -0800

    1

--- models/state.py
+++ models/state.py
@@ -1,35 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
-from models.city import City
-import shlex
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """ State class
-    Attributes:
-        name: The input name
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        list_a = []
-        res = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                list_a.append(var[key])
-        for el in list_a:
-            if (el.state_id == self.id):
-                res.append(el)
-        return (res)
+    """ State class """
+    name = ""
