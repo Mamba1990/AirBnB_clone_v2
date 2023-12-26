9c951df4ddd9dc88391e30f2fbb2844ece3a2307
Mamba1990 <h.daoudim@outlook.com>
2023-12-26 07:42:52 -0800

    9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Starting a Flask web app """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def dispose(exception):
+    """ Removing current session """
+    storage.close()
+
+
+@app.route('/cities_by_states')
+def states():
+    """ Displaying list of all the states """
+    states = storage.all(State)
+    statesList = list(states.values())
+    return render_template('8-cities_by_states.html', states=statesList)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
