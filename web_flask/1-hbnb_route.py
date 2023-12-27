043d71cb5b42bf3ae34f4a6e706b212f055a78a0
Mamba1990 <h.daoudim@outlook.com>
2023-12-27 11:13:17 -0800

    pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,10 +7,12 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_hbnb():
     """Displaying 'Hello HBNB!' on the index page"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
