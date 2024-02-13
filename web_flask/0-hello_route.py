51d7f6d277a24ab91e020e15c72115b82e111ce6
Mamba1990 <h.daoudim@outlook.com>
2024-02-13 03:39:57 -0800

    right

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""Starting a Flask web application"""
-
-
-# Importing Flask class from flask module
+""" Starts a Flash Web Application """
 from flask import Flask
-
-# Creation an instance called app of the class by passong the __name__ variable
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """Displaying "Hello HBNB!"
-
-    Returns:
-        str: The index page's text
-    """
+def hello_hbnb():
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
+
